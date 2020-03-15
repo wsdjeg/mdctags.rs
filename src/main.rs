728053840576afd5d06f51826d64fcc4a48965de
Shidong Wang <wsdjeg@outlook.com>
2020-03-15 18:01:12 +0800

    Update

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::fs;
 use std::process;
 fn main() {
     let args: Vec<String> = std::env::args().collect();
+    println!("{:?}", "### intro".find(' ').expect(""));
+    // process::exit(0);
     println!("!_TAG_FILE_FORMAT       2       /extended format; --format=1 will not append ;\" to lines/'
 !_TAG_FILE_SORTED       0       /0=unsorted, 1=sorted, 2=foldcase/'
 !_TAG_PROGRAM_AUTHOR    wsdjeg /wsdkeg@outlook.com/'
@@ -13,6 +15,7 @@ fn main() {
         process::exit(0);
     }
     let contents = fs::read_to_string("README.md").expect("");
+    let mut stack: Vec<Line> = Vec::new();
     let mut in_code = false;
     let mut line_no = 0;
     for line in contents.lines() {
@@ -21,8 +24,24 @@ fn main() {
             in_code = !in_code;
         }
         if !in_code && line.starts_with("#") {
-            let title: String = line.split_whitespace().collect()[1];
+            let item: Line = Line::split(line);
             println!("{}", line);
+        }
+    }
+}
+
+struct Line {
+    title: String,
+    level: i32,
+}
+
+impl Line {
+    fn split(line: &str) -> Line {
+        let idx = line.find(' ').unwrap();
+        Line {
+            // @question 如何从字符串取切片并转换为 String
+            title: line.chars().collect(),
+            level: idx as i32,
         }
     }
 }
